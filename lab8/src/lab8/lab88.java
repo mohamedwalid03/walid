6e9c243d184d600da5b4aac3ed99926d46a2b826
mohamed walid <mw455346@gmail.com>
2020-04-26 21:14:13 +0200

    hallo version

---
+++ lab8/src/lab8/lab88.java
@@ -0,0 +1,10 @@
+package lab8;
+
+public class lab88 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         System.out.print("mohamed walid 20180244");
+	}
+
+}
